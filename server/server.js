9c8a30830e476a8527f532d2f4c9e9773fe02102
Abhilash S <abhilashsubhash72@gmail.com>
2025-11-01 18:34:47 +0530

    Connected mongoDB

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import connectDB from './configs/db.js';
 
 const app = express();
+await connectDB();
 
 // Middleware
 app.use(cors());
@@ -13,7 +15,7 @@ app.get('/', (req, res) => {
   res.send('API is working');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log('Server is running on port' + PORT);
