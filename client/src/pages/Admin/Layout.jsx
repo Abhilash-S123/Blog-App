0eadc302b67e24d8e9bbb8f048cda8a6285162fc
Abhilash S <abhilashsubhash72@gmail.com>
2025-11-08 12:20:24 +0530

    Added logout feature

--- client/src/pages/Admin/Layout.jsx
+++ client/src/pages/Admin/Layout.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { assets } from '../../assets/assets'
 import { Outlet, useNavigate } from 'react-router-dom'
 import Sidebar from '../../components/Admin/Sidebar';
+import { useAppContext } from '../../context/AppContext';
 
 const Layout = () => {
 
-    const navigate = useNavigate();
+    //const navigate = useNavigate();
+
+    const {axios, setToken, navigate} = useAppContext()
 
     const logout = () => {
+        localStorage.removeItem('token')
+        axios.defaults.headers.common['Authorization'] = null;
+        setToken(null)
         navigate('/');
     }
   return (
@@ -22,7 +28,7 @@ const Layout = () => {
        <div className='flex h-[ calc(100vh-70px)]'>
              <Sidebar />
              <Outlet />
-       </div>
+       </div>  
       
     </>
   )
