7ce1933b6889e5cafed4a767629c1c265ac22269
Abhilash S <abhilashsubhash72@gmail.com>
2025-11-08 11:36:19 +0530

    Added functionality to approve and delete comment

--- client/src/pages/Admin/CommentTableitem.jsx
+++ client/src/pages/Admin/CommentTableitem.jsx
@@ -1,10 +1,46 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
+import { useAppContext } from '../../context/AppContext';
+import toast from 'react-hot-toast';
 
 const CommentTableitem = ({comment, fetchComments}) => {
 
    const {_id, blog, createdAt} = comment;
    const BlogDate = new Date(createdAt);
+
+   const {axios} = useAppContext()
+
+   const approveComment = async () => {
+    try {
+        const {data} = await axios.post('/api/admin/approve-comment', {id: _id})
+    if (data.success) {
+        toast.success(data.message)
+        fetchComments()
+    } else {
+        toast.success(data.message) 
+    }
+    } catch (error) {
+        toast.success(error.message) 
+    }
+   }
+
+const deleteComment = async () => {
+    try {
+        const confirm = window.confirm('Are you sure you want to delete this comment ?')
+        if(!comment) return;
+
+        const {data} = await axios.post('/api/admin/delete-comment', {id: _id})
+            if (data.success) {
+               toast.success(data.message)
+               fetchComments()
+            } else {
+          toast.success(data.message) 
+         }  
+    } catch (error) {
+        toast.success(error.message) 
+    }
+   }
+    
 
   return (
          <tr className='order-y border-gray-300'> 
@@ -21,10 +57,12 @@ const CommentTableitem = ({comment, fetchComments}) => {
             </td>
             <td className='px-6 py-4'>
                  <div className='inline-flex items-center gap-4'>
-                    {!comment.isApproved ? <img className='w-5 hover:scale-110 transition-all cursor-pointer'
+                    {
+                    !comment.isApproved ? 
+                    <img onClick={approveComment} className='w-5 hover:scale-110 transition-all cursor-pointer'
                      src={assets.tick_icon} alt="" /> : <p className='text-green-600 px-3 text-xs border border-green-600 
                      bg-green-100 rounded-full py-1'>Approved</p>}        
-                      <img className='w-5 hover:scale-110 transition-all cursor-pointer' src={assets.bin_icon} alt="" />     
+                      <img onClick={deleteComment} className='w-5 hover:scale-110 transition-all cursor-pointer' src={assets.bin_icon} alt="" />     
                  </div>
             </td>
          </tr>
